a6c024ccd3fb2bd3f8aeaa9cbd4cbea3bd850001
nelson valdivia <nperez88@gmail.com>
2016-04-04 16:39:31 -0700

    uses store_api_results to scrape and store all API results into a file to use as seeds

--- store_api_results.js
+++ store_api_results.js
@@ -1,5 +1,4 @@
 var request = require('request');
-var locus = require('locus');
 
 var parsedreturn;
 
@@ -7,8 +6,7 @@ var parsedreturn;
 for(i=1;i<101;i++){
   request.get('http://xkcd.com/' + i + '/info.0.json', function(err,res,body) {
     parsedreturn = JSON.parse(body);
-    console.log(parsedreturn);
-    eval(locus);
+    console.log(parsedreturn, ",");
   });
 
 };
