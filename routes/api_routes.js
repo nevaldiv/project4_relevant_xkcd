1deeb1ffa36c21e22d4073e9b63c6f3a1250b3ae
nelson valdivia <nperez88@gmail.com>
2016-04-07 12:50:35 -0700

    adds comic api routes in preparation for READ and UPDATE functionality

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -2,10 +2,16 @@ var express = require('express'),
     router  = new express.Router();
 
 // Require controllers.
-var usersCtrl = require('../controllers/users');
+var usersCtrl  = require('../controllers/users');
+var comicsCtrl = require('../controllers/comics');
 
 // Require token authentication.
 var token = require('../config/token_auth');
+
+//comics resource paths:
+router.get('/comics', comicsCtrl.comicIndex);
+router.put('/comics/:id', comicsCtrl.comicEdit);
+
 
 // users resource paths:
 router.post('/users',    usersCtrl.create);
@@ -16,3 +22,4 @@ router.post('/token', token.create);
 router.post('/users/me/token', token.authenticate, token.refresh);
 
 module.exports = router;
+
