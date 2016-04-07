33b25a21007a66be59e185abb75d19f7670fd75e
nelson valdivia <nperez88@gmail.com>
2016-04-07 13:39:06 -0700

    adds comics controller to git tracking forealzies this time

--- controllers/comics.js
+++ controllers/comics.js
@@ -1,1 +1,38 @@
-comics.js
+var Comic = require('../models/comic');
+
+//||||||||||||||||||||||||||--
+// GET COMICS
+//||||||||||||||||||||||||||--
+var comicIndex = function(req, res) {
+  Comic.find({}, function(err, comics) {
+    if (err) {
+      res.send(err);
+    }
+
+    // return the comic as JSON
+    res.json(comics);
+    console.log(comics);
+  });
+}
+
+var comicEdit = function(req,res){
+  var id = req.params.id;
+  Comic.findById(id, function(err,comic){
+    if(err) res.send(err);
+    if(req.body.votecount != comic.votecount) comic.votecount = parseInt(req.body.votecount);
+    comic.save(function(err, updatedcomic){
+      if(err) res.send(err);
+      console.log("Comic votecount changed!");
+      res.json(updatedcomic);
+    })
+  })
+}
+
+
+
+// Export the function/s as JSON
+module.exports = {
+  // comicShow:   comicShow,
+  comicIndex:  comicIndex,
+  comicEdit:   comicEdit
+}
