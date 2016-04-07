7f4ece905a00f79d7c376251d563fd64be60dbc8
nelson valdivia <nperez88@gmail.com>
2016-04-07 13:03:58 -0700

    adds getComics function to comicService for SHOW, exports functionality for controller

--- public/js/app/comics/comics.service.js
+++ public/js/app/comics/comics.service.js
@@ -8,14 +8,19 @@
 
   function comicData($log, $http) {
 
+    //Hits the backend API and gets me my comics for SHOW
+    function getComics(){
+      var  promise = $http({
+        method: 'GET',
+        url: '/api/comics'
+      });
+      return promise;
+    }
 
 
-
-    var service = {};
-
+    var service = {
+      show:  getComics
+    };
     return service;
-
-
   };
-
 })();
