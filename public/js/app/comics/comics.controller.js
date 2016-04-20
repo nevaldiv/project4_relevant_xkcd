6cd349125b453d2a84d31c237596793bc39795e9
nelson valdivia <nperez88@gmail.com>
2016-04-19 23:54:26 -0700

    adds new custom filter to be able to filter by year published
    added checkboxes to additionally filter by year published, since the search is really a psuedosearch, it was the next greatest specifier.
    this change included: adding a working array for the years selected to be pushed to, and for the filter to check if any of those were called to use by the user.
    
    additonally, commit adds: jquery functionality, for future styling planned.

--- public/js/app/comics/comics.controller.js
+++ public/js/app/comics/comics.controller.js
@@ -1,17 +1,14 @@
 (function() {
   'use strict';
-
   angular.module('app')
     .controller('ComicController', ComicController);
 
   ComicController.$inject = ["comicData", "$http", "$log"];
 
   function ComicController(comicData, $http, $log) {
-
     $log.info("ComicController linked!");
     var vm = this;
-
-
+    //show the comics function on pageload
     vm.getComics = function() {
       comicData
       .show()
@@ -20,10 +17,33 @@
         vm.comics = comics.data;
       });
     }
-
     vm.getComics();
 
+    vm.yearIncludes = [];
+    vm.includeYear = function(year) {
+        // var i = vm.yearIncludes.indexOf(year);
+        var i = $.inArray(year, vm.yearIncludes);
+        //laymans expl of below: according to array.indexOf MDN Docs, if the year is present
+        //in the array, itll return its index #, if not, returns -1  -  <3 nelson
+        if (i > -1) {
+            //if its there, take it out!
+            vm.yearIncludes.splice(i, 1);
+        } else {
+            //if not, put it in :)
+            vm.yearIncludes.push(year);
+        }
+    }
+    vm.yearFilter = function(comic) {
+        if (vm.yearIncludes.length > 0) {
+          // if (vm.yearIncludes.indexOf(year))
+          if ($.inArray(comic.year, vm.yearIncludes) < 0)
+              return;
+        }
+        return comic;
+    }
 
+
+    //upvote functionality
     vm.updateComic = function(comic, votecountBoolean){
       if (votecountBoolean === false) {
         comic.votecount--;
@@ -38,6 +58,8 @@
           $log.info(err);
         })
     }
+
+
 
   }
 
